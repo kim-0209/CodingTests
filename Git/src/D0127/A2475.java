1dd6bd1beee145c128a1795da636479db2351916
kimseungjin1234 <ktk5594@naver.com>
2023-01-27 18:13:56 +0900

    beakjoon study 2475

--- Git/src/D0127/A2475.java
+++ Git/src/D0127/A2475.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class A2475 {
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in); //스캐너
 
-		int result = 0;
+		int result = 0; // 값 담을 곳
 		
-		for (int i = 0; i < 5; i++) {
-			int a = sc.nextInt();
-			result += a*a;
+		for (int i = 0; i < 5; i++) { // 5개 숫자 받기
+			int a = sc.nextInt(); // 고유번호 하나씩 받기
+			result += a*a; // 고유번호 제곱
 		}
-		System.out.println(result % 10);
+		System.out.println(result % 10); // 제곱한 값 10으로 나눈 값 출력
 	}
 }
