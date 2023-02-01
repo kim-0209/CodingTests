3a15e59a1477ab576d7ba19105b39efae1932de0
kimseungjin1234 <ktk5594@naver.com>
2023-02-01 19:39:17 +0900

    baekjoon 2743 study

---
+++ Git/src/D0201/A2743.java
@@ -0,0 +1,13 @@
+package D0201;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class A2743 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print(br.readLine().length());
+
+	}
+}
